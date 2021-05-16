e736481a1da56cdcce638491d58c870aa5a274bd
User <User@LAPTOP-128AKL1H>
2021-05-16 15:57:03 -0600

    Hice Muchos cambios

--- Proyecto/src/Main/FXMLMenuSesionController.java
+++ Proyecto/src/Main/FXMLMenuSesionController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto;
+package Main;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author User
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLMenuSesionController implements Initializable {
     
     @FXML
     private Label label;
